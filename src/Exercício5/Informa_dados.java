990927c5a685533f06ed9c5f54cb21bb2eba03ee
Erica Marcondes <erica.elen@live.com>
2023-08-25 22:05:34 -0300

    Modificação para inserção de dados pelos usuários

--- src/Exercício5/Informa_dados.java
+++ src/Exercício5/Informa_dados.java
@@ -1,12 +1,23 @@
 package Exercício5;
 
+import javax.swing.JOptionPane;
 public class Informa_dados {
     public static void main (String [] args) {
+
         Dados_Contribuinte a = new Dados_Contribuinte();
-        a.setNome("Érica Marcondes");
-        a.setCPF("000.000.000-01");
-        a.setCNPJ("000.000.00/0001");
-        a.setCartao("00.000.000-X");
+
+        String nm, cpf, cnpj, ct;
+
+        nm = JOptionPane.showInputDialog(null, "Insira o nome: ");
+        cpf = JOptionPane.showInputDialog(null, "Insira o CPF: ");
+        cnpj = JOptionPane.showInputDialog(null, "Insira o CNPJ: ");
+        ct = JOptionPane.showInputDialog(null, "Insira o RG: ");
+
+        a.setNome(nm);
+        a.setCPF(cpf);
+        a.setCNPJ(cnpj);
+        a.setCartao(ct);
+
         System.out.println("Nome do contribuinte: " + a.getNome());
         System.out.println("CPF do contribuinte: " + a.getCPF());
         System.out.println("CNPJ do contribuinte: " + a.getCNPJ());
