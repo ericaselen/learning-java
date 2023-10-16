a2ef351917412aabc12c15cdf5cda93d1d259bb1
érica <erica.elen@live.com>
2023-10-16 10:46:51 -0300

    exercicio8 interface poo

---
+++ src/Exercício8/Dados_Contribuinte.java
@@ -0,0 +1,40 @@
+package Exercício8;
+
+public class Dados_Contribuinte {
+    private String nome;
+    private String CPF;
+    private String CNPJ;
+    private String cartao;
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return this.CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public String getCNPJ() {
+		return this.CNPJ;
+	}
+
+	public void setCNPJ(String CNPJ) {
+		this.CNPJ = CNPJ;
+	}
+
+	public String getCartao() {
+		return this.cartao;
+	}
+
+	public void setCartao(String cartao) {
+		this.cartao = cartao;
+	}
+}
